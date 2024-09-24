c0d257eaa5741da00f478996075f8772d92cab2f
Blessing Denis <fargoxyt@gmail.com>
2024-09-24 17:07:22 +0100

    added shopping list

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,25 @@
+def display_menu():
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View list")
+    print("4. Exit")
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            shopping_list.append(input("Enter item: "))
+            pass
+        elif choice == "2":
+            shopping_list.remove(input("Enter item to remove: "))
+            pass
+        elif choice == "3":
+            print(shopping_list)
+            pass
+        elif choice == "4":
+            print("Goodbye")
+            break
+        else:
+            print("Invalid choice. Please try again")
+main()
